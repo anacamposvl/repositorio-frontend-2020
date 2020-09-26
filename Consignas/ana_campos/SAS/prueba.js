7b53f33c2a4cdf703f02cd142b95bfd7e57f42dc
¨Valentina¨ <anacamposlozada@gmail.com>
2020-09-26 09:30:42 -0300

    agregue sas

---
+++ Consignas/ana_campos/SAS/prueba.js
@@ -0,0 +1,25 @@
+<script>
+    var nombre;
+    nombre = Ada;
+    var nombre2 = Ada;
+
+    strings = cadena de datos
+    boolean = true o false
+    integers = numeros enteros
+
+    console.log(nombre2);
+
+    //conatenar strings
+    var nombreCompleto = nombre + `` + nombre2;
+    respuesta = ADA ITW
+
+    var datobooleano = true;
+    respuesta = true;
+
+
+    //Operadores aritmeticos 
+    var miEdad = 20;
+    var edadDeMiHermano = 15;
+    var resultado = mi Edad + edadDeMiHermnao;
+    console.log (resultado);
+</script>
